8be93fb7ab92180021411c69c73d56da3a3b80e2
Jefferson Alionco <Jeffersonalionco@gmail.com>
2026-03-10 16:00:01 -0300

    first commit

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const server = http.createServer((req, res) => {
   })
 })
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Landingpage da Comunidade Metaji rodando em http://localhost:${PORT}`)
 })
 
